6050b6d3940ab79e6d0b8843554273ba6dfdd661
iizaghz <sukmahawaiza@gmail.com>
2025-03-22 08:04:05 +0700

    add procedure insertionsort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -32,3 +32,27 @@ void input()
     }
 }
 
+void insertionSort()
+{ // procedure insertionSort
+    
+int temp; // membuat variable data temporer atau penyimpanan sementara
+int j, i; // membuat variable j sebagai penanda
+
+for (i = 1; i <= n - 1; i++)
+    { // step 1
+
+        temp = arr[i]; // step 2
+
+        j = i - 1; // step 3
+
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+        arr[j + 1] = arr[j]; //step 4a
+        j--; // step 4b
+        }
+
+        arr[j + 1] = temp; // step 5
+    }
+}
+
+
